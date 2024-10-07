4c1ae3e3a68824467690e5e389133e4da535d995
gabriellucasvh <gabriel.gopp@gmail.com>
2024-10-07 13:41:44 -0300

    up

--- components/specify/Inicio/index.tsx
+++ components/specify/Inicio/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Inicio = () => {
   return (
     <div
-      className="flex justify-center items-center h-screen bg-black/70 bg-[url('/graos.png')] bg-cover bg-no-repeat bg-center animate-bgMove"
+      className="flex justify-center items-center h-screen bg-[url('/graos.png')] bg-cover bg-no-repeat bg-center animate-bgMove"
     >
-      <div className="flex flex-col justify-start bg-black/60 items-center p-4 w-[80%] h-[70%] rounded-xl">
-        <h1 className="text-9xl text-merino-500 text-center drop-shadow-lg font-bold uppercase">
+      <div className="flex flex-col justify-start items-center p-4 w-[80%] h-[70%] rounded-xl">
+        <h1 className="text-9xl text-yellow-500 text-center drop-shadow-lg font-bold uppercase">
 
 
           Expresso <br /> para você
