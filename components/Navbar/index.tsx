eaa8fc2850aed2c9eecc1723066524a8843272ab
Gabriel Gonçalves <gabriel.gopp@gmail.com>
2024-11-11 23:52:19 -0300

    Responsividade aplicada

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -8,31 +8,19 @@ interface Menu {
 }
 
 const Menus: Menu[] = [
-  {
-    title: "Menu",
-    link: "/",
-  },
-  {
-    title: "Sobre nós",
-    link: "#sobre",
-  },
-  {
-    title: "Produtos",
-    link: "#produtos",
-  },
-  {
-    title: "Contato",
-    link: "#contato",
-  },
+  { title: "Menu", link: "/" },
+  { title: "Sobre nós", link: "#sobre" },
+  { title: "Produtos", link: "#produtos" },
+  { title: "Contato", link: "#contato" },
 ];
 
 const Navbar = () => {
   return (
-    <nav className="flex w-[90%] mx-auto fixed p-4 m-4 justify-between items-center right-0 left-0 h-16 bg-merino-800 z-50 text-white rounded-full">
-      <Link href="/">
-        <Image src="/logo.svg" alt="logo" width={150} height={150} />
+    <nav className="flex w-full max-w-[90%] mx-auto fixed top-0 p-4 justify-between items-center h-16 bg-merino-800 text-white z-50 rounded-full md:justify-center lg:justify-between">
+      <Link href="/" className="flex-shrink-0">
+        <Image src="/logo.svg" alt="logo" width={120} height={120} className="w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16" />
       </Link>
-      <ul className="flex gap-4 items-center">
+      <ul className="hidden md:flex gap-4 items-center">
         {Menus.map((menu, index) => (
           <li key={index}>
             <Link className="hover:text-merino-200 transition duration-300" href={menu.link}>
@@ -41,9 +29,12 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      <button className="bg-white text-merino-800 font-medium px-4 py-2 rounded-full border-2 border-transparent hover:border-2 hover:border-merino-900 shadow-md">
+      <button className="hidden md:inline-block bg-white text-merino-800 font-medium px-4 py-2 rounded-full border-2 border-transparent hover:border-merino-900 shadow-md transition duration-300">
         Comprar agora
       </button>
+      <div className="md:hidden flex items-center">
+        <button className="text-white focus:outline-none hover:text-merino-200">☰</button>
+      </div>
     </nav>
   );
 };
